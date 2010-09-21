72e47a20797933d5f2167a33fcfe920ae26f836e
kr4uzi@gmail.com <kr4uzi@gmail.com@ba25c47e-de5b-bb52-df15-9bdbbe628eaa>
2010-09-21 17:31:19 +0000

    updated BF2_Starter

--- trunk/scripts/BF2_Starter.js
+++ trunk/scripts/BF2_Starter.js
@@ -50,71 +50,67 @@ function SetHostsFile( fso, shell, ip )
     var ip = ( ip == null ) ? "127.0.0.1" : ip;
     var env = shell.Environment( "PROCESS" );
     var hostspath = env( "SYSTEMROOT" ) + "\\System32\\drivers\\etc\\hosts.ics";
-    var hostspath_backup = hostspath + ".backup";
 	
-	try
-	{	
-		if( !FileExists(fso, hostspath) )
-			fso.CreateTextFile( hostspath );
-		else
-		{
-			var hosts_file = fso.GetFile( hostspath );
-			gAttrs = hosts_file.attributes
-			hosts_file.attributes = 0;
-			
-			if( FileExists(fso, hostspath_backup) )
-			{
-				var hosts_file_backup = fso.GetFile( hostspath_backup );
-				hosts_file_backup.attributes = 0;
+    try
+    {	
+        if( !FileExists(fso, hostspath) )
+            fso.CreateTextFile( hostspath );
+        else
+        {
+            var hosts_file = fso.GetFile( hostspath );
+            gAttrs = hosts_file.attributes
+            hosts_file.attributes = 0;
+        }
+        
+        CleanUpHosts( fso, shell );
 				
-				fso.DeleteFile( hostspath );
-				fso.CopyFile( hostspath_backup, hostspath );
-				// delete the old hosts.ics file to renew the entries
-			}
-			else
-				fso.CopyFile( hostspath, hostspath_backup );
-		}
-				
-		var hostsObj = fso.GetFile( hostspath );
-		var hosts = hostsObj.OpenAsTextStream( 8, 0 );
+        var hostsObj = fso.GetFile( hostspath );
+        var hosts = hostsObj.OpenAsTextStream( 8, 0 );
 		
-		hosts.WriteLine( "" ); // empty line - just to play it safe
-		hosts.WriteLine( ip + " bf2web.gamespy.com" );
-		hosts.WriteLine( ip + " gpcm.gamespy.com" );
-		hosts.WriteLine( ip + " gpsp.gamespy.com" );
-		hosts.WriteLine( ip + " battlefield2.available.gamespy.com" );
+        hosts.WriteLine( "" ); // empty line - just to play it safe
+        hosts.WriteLine( ip + " bf2web.gamespy.com #bf2_statistics_line" );
+        hosts.WriteLine( ip + " gpcm.gamespy.com #bf2_statistics_line" );
+        hosts.WriteLine( ip + " gpsp.gamespy.com #bf2_statistics_line" );
+        hosts.WriteLine( ip + " battlefield2.available.gamespy.com #bf2_statistics_line" );
 		
-		hosts.close( );
-	}
-	catch( exception )
-	{
-		throw "Insufficient rights!";
-	}
+        hosts.close( );
+    }
+    catch( exception )
+    {
+        throw "Insufficient rights!";
+    }
 }
 
 function CleanUpHosts( fso, shell )
 {
     var env = shell.Environment( "PROCESS" );
     var hostspath = env( "SYSTEMROOT" ) + "\\System32\\drivers\\etc\\hosts.ics";
-    var hostspath_backup = hostspath + ".backup";
 			
-	try
-	{
-		if( FileExists(fso, hostspath_backup) )
-		{
-			fso.DeleteFile( hostspath );
-			fso.CopyFile( hostspath_backup, hostspath );			
-			fso.DeleteFile( hostspath_backup );
-			
-			// give back hosts.ics its old attributes
-			var hosts_file = fso.GetFile( hostspath );
-			hosts_file.attributes = gAttrs;
-		}
-	}
-	catch( exception )
-	{
-		throw "Insufficient rights!";
-	}
+    try
+    {
+        var file = fso.OpenTextFile( hostspath, 1, true );
+        var content = "";
+            
+        while( !file.AtEndOfStream )
+        {
+            var line = file.ReadLine( );
+                
+            if( line.indexOf("#bf2_statistics_line") == -1 && line.length > 0 )
+                content += line + "\n";
+        }
+        
+        file.close( );
+        
+        file = fso.OpenTextFile( hostspath, 2 );
+        file.WriteLine( content );
+        file.close( );
+        
+        fso.GetFile(hostspath).attributes = gAttrs;
+    }
+    catch( exception )
+    {
+        throw "Insufficient rights!";
+    }
 }
 
 function StartBF2( ) 
@@ -137,19 +133,19 @@ function StartBF2( )
         return -1;
     }
 		
-	try
-	{
-		SetHostsFile( fso, shell );
+    try
+    {
+        SetHostsFile( fso, shell );
 		
-		shell.CurrentDirectory = bf2_dir;
-		shell.Run( "\"" + bf2_dir + "\\BF2.exe\"", 1, true );
+        shell.CurrentDirectory = bf2_dir;
+        shell.Run( "\"" + bf2_dir + "\\BF2.exe\"", 1, true );
 		
-		CleanUpHosts( fso, shell );
-	}
-	catch( exception )
-	{
-		shell.PopUp( exception );
-	}
+        CleanUpHosts( fso, shell );
+    }
+    catch( exception )
+    {
+        shell.PopUp( exception );
+    }
 	
     delete fso;
     delete shell;
